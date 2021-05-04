a99524343445a556a1d706bfc0ffc7cfc3f3044c
HangYang <s3799719@student.rmit.edu.au>
2021-05-04 18:18:53 +0800

    update allocateShiftDialog

--- src/components/Calendar/Calendar.vue
+++ src/components/Calendar/Calendar.vue
@@ -241,6 +241,7 @@ export default {
       // } else {
       //   open()
       // }
+      console.log({event})
       this.selectedEvent = event
       this.selectedElement = nativeEvent.target
       nativeEvent.stopPropagation()
