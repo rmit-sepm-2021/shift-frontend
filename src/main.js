e188dc91f56ef16ddac1436fe0e9c1e236a5b11c
Zhihao Liang <s3798366@student.rmit.edu.au>
2021-05-30 17:55:56 +0800

    sprint1 update

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 import './permission' // permission control
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import('./mock.js')
+
 Vue.config.productionTip = true
 Vue.use(ElementUI);
 new Vue({
